84f8fb7a7232cf4e2c906bce220f32239736848d
joonhyong <dbshwns0528@naver.com>
2026-02-17 05:38:53 +0900

    [HSC-94] style: 글꼴 추가 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,20 @@
 import "./globals.css";
 
+import localFont from "next/font/local";
+
 import { TanstackProvider } from "@/lib/tanstack/provider";
+
+const pretendard = localFont({
+  src: "../assets/fonts/PretendardVariable.woff2",
+  weight: "100 900",
+  variable: "--font-pretendard",
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>
+      <body className={`${pretendard.variable}`}>
         <TanstackProvider>{children}</TanstackProvider>
       </body>
     </html>
