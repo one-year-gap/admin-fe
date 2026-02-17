46dcac314bd19d894cc06e99b7e987cde2587f22
joonhyong <dbshwns0528@naver.com>
2026-02-17 23:59:49 +0900

    [HSC-99] feat: 더미 사이드바, 더미 헤더바 생성

---
+++ src/components/common/DummyHeaderbar.tsx
@@ -0,0 +1,3 @@
+export function DummyHeaderbar() {
+  return <header className="h-16 w-full bg-gray-200" />;
+}
---
+++ src/components/common/DummySideBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DummySideBar = () => {
+  return <aside className="h-screen w-70 bg-gray-200" />;
+};
+
+export default DummySideBar;
