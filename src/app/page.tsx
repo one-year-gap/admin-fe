282dfa87fada04f90915ca9164e55cc15eb93bec
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-02-19 16:44:04 +0900

    [HSC-102] fix: 코멘트 반영하여 layout으로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-"use client";
-import { useState } from "react";
-
-import Header from "@/components/common/Header";
-import SideBar from "@/components/common/SideBar";
-import Chart from "@/components/domain/region/Chart";
-import Map from "@/components/domain/region/Map";
-
 export default function Home() {
-  const [clickedRegion, setClickedRegion] = useState<string | null>(null);
-  return (
-    <div className="flex min-h-screen">
-      <SideBar />
-      <div className="ml-[280px] flex flex-1 flex-col">
-        <Header pageName="고객 통합 관리" userName="관리자 님" />
-        <main className="flex h-full w-full overflow-hidden bg-neutral-200">
-          <Map selectedRegion={clickedRegion} onSelect={setClickedRegion} />
-          <Chart regionName={clickedRegion} />
-        </main>
-      </div>
-    </div>
-  );
+  return <div>홈</div>;
 }
