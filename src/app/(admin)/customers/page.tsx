0bcf29b9e99f664bba20692e9c1a9f5a177d4a65
joonhyong <dbshwns0528@naver.com>
2026-02-20 02:39:08 +0900

    [HSC-99] feat: 고객 관리 페이지 생성

--- src/app/(admin)/customers/page.tsx
+++ src/app/(admin)/customers/page.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import { SearchBar } from "@/components/common/SearchBar";
+import { CustomersList } from "@/components/domain/customers/CustomersList ";
+import { CustomersToolBar } from "@/components/domain/customers/CustomersToolBar ";
+import { DataUsageChartCard } from "@/components/domain/customers/DataUsageChartCard ";
+import { GradeChartCard } from "@/components/domain/customers/GradeChartCard ";
 
 export default function CustomersPage() {
-  return <div></div>;
+  return (
+    <>
+      {/* 검색바 */}
+      <section className="col-span-12">
+        <SearchBar />
+      </section>
+
+      {/* 필터바 */}
+      <section className="col-span-12">
+        <CustomersToolBar />
+      </section>
+
+      {/* 고객목록 영역 */}
+      <section className="col-span-12">
+        <CustomersList />
+      </section>
+
+      {/* 차트 영역 */}
+      <section className="col-span-12 md:col-span-6">
+        <DataUsageChartCard />
+      </section>
+
+      <section className="col-span-12 md:col-span-6">
+        <GradeChartCard />
+      </section>
+    </>
+  );
 }
