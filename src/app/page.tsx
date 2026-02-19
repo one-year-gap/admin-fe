2d79809fd87e33dd9588a02f829cff247dad8013
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-02-19 13:40:53 +0900

    [HSC-78] feat: Header 컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Header from "@/components/common/Header";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <p className="text-3xl">test1</p>
       <p className="text-2xl">test2</p>
       <p className="text-xl">test3</p>
+      <Header />
     </div>
   );
 }
