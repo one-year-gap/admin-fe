413d34bb60be6a90135924388c5052ba1e500148
joonhyong <dbshwns0528@naver.com>
2026-02-21 01:52:16 +0900

    [HSC-99] refactor: 지역 통계 페이지 레이아웃 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,22 +9,22 @@ export default function Header({
   userName = "관리자 님",
 }: HeaderProps) {
   return (
-    <header className="bg-neutral-0 sticky top-0 z-10 flex h-22 items-center">
-      <div className="flex w-full items-center justify-between">
-        <div className="py-5 pl-9 text-xl font-bold text-neutral-900">{pageName}</div>
+    <header className="bg-neutral-0 flex h-22 items-center justify-between border-b border-neutral-300 px-10">
+      <div className="text-xl font-bold text-neutral-900">{pageName}</div>
 
-        <div className="flex items-center gap-4">
+      <div className="flex items-center gap-6">
+        <div className="flex items-center justify-center gap-4">
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-300">
-            <UserLock width={32} height={32} />
+            <UserLock className="h-8 w-8" />
           </div>
           <span className="text-md font-semibold text-neutral-900">{userName}</span>
-
-          <button
-            type="button"
-            className="mr-10 bg-neutral-300 px-6 py-3 text-xs font-semibold text-neutral-900">
-            로그아웃
-          </button>
         </div>
+
+        <button
+          type="button"
+          className="bg-neutral-300 px-6 py-3 text-xs font-semibold text-neutral-900">
+          로그아웃
+        </button>
       </div>
     </header>
   );
