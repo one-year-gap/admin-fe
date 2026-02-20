0c1ff035697d69c696588742df1267ac6ef1b8b5
joonhyong <dbshwns0528@naver.com>
2026-02-21 01:51:35 +0900

    [HSC-99] refactor: app/(admin)/layout.tsx 레이아웃 조정

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -5,24 +5,23 @@ import SideBar from "@/components/common/SideBar";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-neutral-0 min-h-screen">
-      <div className="flex h-screen">
-        {/* Sidebar column */}
-        <div className="w-70 shrink-0">
-          <SideBar />
-        </div>
+    <div className="bg-neutral-0 flex h-screen overflow-hidden">
+      {/* 1. 사이드바 */}
+      {/* <DummySideBar /> */}
+      <SideBar />
 
-        {/* Main column */}
-        <div className="flex min-h-0 flex-1 flex-col">
-          {/* Header */}
-          <Header />
+      {/* 2. 메인 영역 */}
+      <div className="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
+        {/* div 태그에  min-h-0 <- 필요 시 추가 */}
+        {/* 3. 헤더바 */}
+        {/* <DummyHeaderBar /> */}
+        <Header />
 
-          {/* Content */}
-          <main className="min-h-0 flex-1 overflow-y-auto bg-neutral-100 p-6">{children}</main>
-          {/* <main className="flex-1 bg-neutral-100 p-6">
+        {/* 4. 페이지 콘텐츠 */}
+        <main className="flex-1 overflow-y-auto bg-neutral-100 p-6">{children}</main>
+        {/* <main className="flex-1 bg-neutral-100 p-6">
             <div className="grid grid-cols-12 gap-6">{children}</div>
           </main> */}
-        </div>
       </div>
     </div>
   );
