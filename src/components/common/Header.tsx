a0ec71c26f1c1f92a0764559a0674319576ea8bf
joonhyong <dbshwns0528@naver.com>
2026-02-21 15:21:24 +0900

    [HSC-99] chore: 헤더바 컴포넌트 충돌 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,6 @@
 import { usePathname } from "next/navigation";
 
 import { UserLock } from "lucide-react";
-
 interface HeaderProps {
   userName?: string;
 }
@@ -24,16 +23,16 @@ export default function Header({ userName = "관리자 님" }: HeaderProps) {
         <div className="py-5 pl-9 text-xl font-bold text-neutral-900">{pageName}</div>
         <div className="flex items-center gap-4">
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-300">
-            <UserLock className="h-8 w-8" />
+            <UserLock width={32} height={32} />
           </div>
           <span className="text-md font-semibold text-neutral-900">{userName}</span>
-        </div>
 
-        <button
-          type="button"
-          className="bg-neutral-300 px-6 py-3 text-xs font-semibold text-neutral-900">
-          로그아웃
-        </button>
+          <button
+            type="button"
+            className="mr-10 bg-neutral-300 px-6 py-3 text-xs font-semibold text-neutral-900">
+            로그아웃
+          </button>
+        </div>
       </div>
     </header>
   );
