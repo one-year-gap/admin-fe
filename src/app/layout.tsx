a1da8493740d50e16521137afb25ef263ff848a4
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-02-21 11:42:20 +0900

    [HSC-120] refactor: 레이아웃 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 
 import localFont from "next/font/local";
 
-import Header from "@/components/common/Header";
-import SideBar from "@/components/common/SideBar";
 import { TanstackProvider } from "@/lib/tanstack/provider";
 
 const pretendard = localFont({
@@ -17,15 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <body className={`${pretendard.variable}`}>
-        <TanstackProvider>
-          <div className="flex h-screen w-full overflow-hidden">
-            <SideBar />
-            <div className="flex flex-1 flex-col">
-              <Header />
-              <main className="overflow-aut flex-1 bg-neutral-200">{children}</main>
-            </div>
-          </div>
-        </TanstackProvider>
+        <TanstackProvider>{children}</TanstackProvider>
       </body>
     </html>
   );
