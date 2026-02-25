8d50df07b2d70a44e46a68c2274e96eb58437386
joonhyong <dbshwns0528@naver.com>
2026-02-25 22:43:39 +0900

    [HSC-151] feat: 이탈 위험 감지 페이지 컴포넌트 및  검색바 컴포넌트 생성

---
+++ src/components/domain/churn/ChurnChart.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ChurnChart() {
+  return <div>이탈 위험 고객 증감 추이 차트</div>;
+}
---
+++ src/components/domain/churn/ChurnFeed.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ChurnFeed() {
+  return <div>실시간 이탈 위험 고객 피드</div>;
+}
