a463c1b119560a2707b335dbd01991a09d375afb
joonhyong <dbshwns0528@naver.com>
2026-02-26 03:37:17 +0900

    [HSC-151] feat: 이탈 위험군 테이블 컴포넌트 생성

--- src/app/(admin)/churn/page.tsx
+++ src/app/(admin)/churn/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 
 import { ChurnChart } from "@/components/domain/churn/ChurnChart";
 import { ChurnFeed } from "@/components/domain/churn/ChurnFeed";
+import { CustomersList } from "@/components/domain/churn/list/ChurnList";
 import { SearchBar } from "@/components/domain/churn/SearchBar";
 
 export default function ChurnPage() {
@@ -17,7 +18,9 @@ export default function ChurnPage() {
       </section>
 
       {/* 고객목록 */}
-      <section className="col-span-12">고객 목록</section>
+      <section className="col-span-12">
+        <CustomersList keyword={keyword} />
+      </section>
 
       {/* 차트 */}
       <section className="col-span-12 md:col-span-6">
