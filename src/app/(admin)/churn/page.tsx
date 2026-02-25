9c584b756908ac54bffbd24d972fb5e441ad96be
joonhyong <dbshwns0528@naver.com>
2026-02-25 22:43:39 +0900

    [HSC-151] feat: 이탈 위험 감지 페이지 컴포넌트 및  검색바 컴포넌트 생성

---
+++ src/app/(admin)/churn/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+
+import { ChurnChart } from "@/components/domain/churn/ChurnChart";
+import { ChurnFeed } from "@/components/domain/churn/ChurnFeed";
+import { SearchBar } from "@/components/domain/churn/SearchBar";
+
+export default function ChurnPage() {
+  const [keyword, setKeyword] = useState("");
+
+  return (
+    <>
+      {/* 검색바 */}
+      <section className="col-span-12">
+        <SearchBar value={keyword} onChange={setKeyword} />
+      </section>
+
+      {/* 고객목록 */}
+      <section className="col-span-12">고객 목록</section>
+
+      {/* 차트 */}
+      <section className="col-span-12 md:col-span-6">
+        <ChurnChart />
+      </section>
+      <section className="col-span-12 md:col-span-6">
+        <ChurnFeed />
+      </section>
+    </>
+  );
+}
