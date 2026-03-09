c468effe23a7ca04f7a443a920522c8a1577223c
joonhyong <dbshwns0528@naver.com>
2026-03-09 17:34:10 +0900

    [HSC-161] feat: 검색바 돋보기 버튼에 검색 기능 추가

--- src/components/domain/customers/search/SearchInput.tsx
+++ src/components/domain/customers/search/SearchInput.tsx
@@ -2,16 +2,20 @@
 
 import { Search } from "lucide-react";
 
+import { cn } from "@/lib/utils";
+
 type SearchInputProps = {
   value: string;
   onChange: (next: string) => void;
+  onSearch: () => void;
 };
 
-export function SearchInput({ value, onChange }: SearchInputProps) {
+export function SearchInput({ value, onChange, onSearch }: SearchInputProps) {
   return (
-    <div className="relative max-w-60">
+    <div className="relative min-w-md">
       <button
         type="button"
+        onClick={onSearch}
         className="absolute top-0 bottom-0 left-0 flex cursor-pointer items-center pl-4"
         aria-label="검색">
         <Search className="text-primary-500 size-5" />
@@ -20,8 +24,17 @@ export function SearchInput({ value, onChange }: SearchInputProps) {
       <input
         value={value}
         onChange={(e) => onChange(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onSearch();
+          }
+        }}
         placeholder="고객명, 연락처로 검색 ..."
-        className="bg-neutral-0 placeholder:text-md focus-visible:border-primary-500 focus:border-primary-500 text-md h-auto w-full truncate rounded-lg border border-neutral-300 py-3 pr-3 pl-12 font-medium shadow-none transition-colors ease-in-out placeholder:truncate placeholder:font-normal placeholder:text-neutral-500 focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+        className={cn(
+          "bg-neutral-0 text-md h-auto w-full truncate rounded-lg border border-neutral-300 py-3 pr-3 pl-12 font-medium shadow-none transition-colors ease-in-out",
+          "placeholder:text-md placeholder:truncate placeholder:font-normal placeholder:text-neutral-500",
+          "focus-visible:border-primary-500 focus:border-primary-500 focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0",
+        )}
       />
     </div>
   );
