f9a088ee587d3a00bcb746cfffc07980e5b629d6
joonhyong <dbshwns0528@naver.com>
2026-03-19 19:18:29 +0900

    [HSC-389] fix: 고객 관리 상세 모달 API 연동 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -16,18 +16,7 @@ export default function Header() {
     <header className="bg-neutral-0 flex h-22 w-full flex-shrink-0 items-center">
       <div className="flex w-full items-center justify-between">
         <div className="py-5 pl-9 text-xl font-bold text-neutral-900">{pageName}</div>
-        <div className="flex items-center gap-4">
-          {/* <div className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-300">
-            <UserLock width={32} height={32} />
-          </div> */}
-          {/* <span className="text-md font-semibold text-neutral-900">{userName}</span>
-
-          <button
-            type="button"
-            className="mr-10 bg-neutral-300 px-6 py-3 text-xs font-semibold text-neutral-900">
-            로그아웃
-          </button> */}
-        </div>
+        <div className="flex items-center gap-4"></div>
       </div>
     </header>
   );
